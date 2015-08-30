8660f6a2ead1bc87a942ec85512bca392b83366f
Kim, Hirokuni <yangkookkim@gmail.com>
2015-08-30 19:34:51 +0900

    wip

--- import_pictures.sh
+++ import_pictures.sh
@@ -4,14 +4,15 @@ for image in $(git ls-files . --exclude-standard --others | grep large)
 do
     date=$(exiftool -q -S -DateCreated $image | awk '{print $2}' | sed 's/:/-/g')
     image_base=$(basename $image)
-    markdown="_posts/${date}.markdown"
+    markdown="_posts/${date}-photo.markdown"
     cat << EOT >> $markdown
 ---
 layout: default
 date:   $date 00:00:00
 photo: $image_base
-caption_header: 実家の魚とばーちゃんの豆料理
-caption: 豆うまかった。がんもはあんまりうまくなかった。
+caption_header:
+caption: 
 ---
 EOT
+    cp -f $image img/thumb/$image_base
 done
